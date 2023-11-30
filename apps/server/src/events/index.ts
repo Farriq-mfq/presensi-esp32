355c4d36741a977d5e03c039e73da39258fc08bf
Farriq muwaffaq <farriqmuwaffaq100@gmail.com>
2023-11-30 15:01:58 +0700

    feat: add socket io

---
+++ apps/server/src/events/index.ts
@@ -0,0 +1,5 @@
+const events = {
+    TEST: ''
+}
+
+export default events 
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import { env } from "./env";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express()
 
-app.get("/", function (req, res) {
-    res.send('tset serer')
+const server = createServer(app)
+const io = new Server(server, {})
+
+
+io.on('connection', (socket) => {
+    console.log("CLIENT CONNECTED")
 })
-app.listen(env.PORT, () => {
-    console.log(`SERVER RUNNING AT : http://localhost:${env.PORT}`)
+
+server.listen(env.PORT, () => {
+    console.log(`SERVER RUNNING AT : ws://localhost:${env.PORT}`)
 })
